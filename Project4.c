54f0c25355a238037cf71a888e66613c8885d8d4
Brandon Gipson <gipsonb@flip2.engr.oregonstate.edu>
2016-06-03 02:34:46 -0700

    Debug I/O and Add Nearest Neighbor Algorithm

--- Project4.c
+++ Project4.c
@@ -1,35 +1,42 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <limits.h>
+
+struct City {
+	long int id;
+	long int x;
+	long int y;
+};
 
 //declare algorithm functions here
 int distance(int xOne, int xTwo, int yOne, int yTwo);
 
-struct City {
-	int id;
-	int x;
-	int y;
-};
-
-
 int main(int argc, char *argv[]) {
-	FILE *inputFile = fopen(argv[2], "r");
-	int i = 0, j = 0, k = 0, l = 0, ans = 0, num, someNumber, n;
-	char *out;
-	struct City cities[n]; //change n to be the number of cities or update this the be dynamic
+	int i = 0, j = 0, k = 0, l = 0, num = 0, shortCityID = 0, totalCities = 0, cityNumber = 0, isVisited = 0, totalCost = 0, current = 0, dCost = INT_MAX;
+	char filename[100];
+	struct City cities[100];
+	int cost[100][100];
+	int path[100];
+	int pathSize;
+
+	memset(path, 0, sizeof(path[100] * 0));
+	memset(cost, 0, sizeof(cost[0][0]) * 100 * 100);
 	
-	strcpy(out, argv[2]);
-	strcat(out, ".tour");
+	strcpy(filename, argv[1]);
+	FILE *inputFile = fopen(filename, "r");
+	
+	strcat(filename, ".tour");
 	
 	while(fscanf(inputFile, "%d", &num) == 1) {
-		k = i - 1;
+		k = i + 1;
 		
 		if(k % 3 == 0) {
 			cities[j].y = num;
 			j++;
 		}
 		
-		else if(k % 2 == 0 && k % 3 != 0) {
+		else if((k % 2 == 0) && (k % 3 != 0)) {
 			cities[j].x = num;
 		}
 		
@@ -37,43 +44,79 @@ int main(int argc, char *argv[]) {
 			cities[j].id = num;
 		}
 		
-		i++;
+		if(k == 3) {
+			i = 0;
+		}
+		
+		else {
+			i++;
+		}
+		
 	}
 	fclose(inputFile);
 	
+	//print cities for testing
+	/*
+	for(i = 0; i < j; i++) {
+		printf("City ID: %d (%d, %d) \n", cities[i].id, cities[i].x, cities[i].y);
+	}*/
+	
 	//Cost matrix creater. Ex. cost[2][4] = the distance from cities[2].id to cities[4].id
-	int cost[j][j];
+	
+	totalCities = j;
+	
 	for(i = 0; i <= j; i++) {
 		for(k = 0; k < j; k++) {
 			cost[i][k] = distance(cities[i].x, cities[k].x, cities[i].y, cities[k].y);
 		}
 	}
 	
+	//print cost matrix for testing
+	/*
+	for(i = 0; i <= j; i++) {
+		for(k = 0; k < j; k++) {
+			printf("[%d][%d] %d ", i, k, cost[i][k]);
+		}
+		printf("\n\n");
+	}*/
 	
-	if(j < someNumber ) { //change someNumber to real value
-		//call algorithm
+	path[0] = cities[0].id;
+	pathSize = 1;
+	
+	while(pathSize < totalCities) {
+		for(j = 0; j < totalCities; j++) {
+			if(cost[current][j-1] < dCost) {
+				for(k = 0; k < pathSize; k++) {
+					if(cities[j-1].id == path[k]) {
+						isVisited = 0;
+					}
+					else {
+						isVisited = 1;
+					}
+				}
+				if(isVisited == 1) {
+					dCost = cost[current][j-1];
+					cityNumber = j;
+					shortCityID = cities[j-1].id;
+				}
+			}
+		}
+		totalCost += dCost;
+		dCost = INT_MAX;
+		current =  cityNumber;
+		path[pathSize] = shortCityID;
+		pathSize++;
 	}
 	
-	else if(j > someNumber && j < someNumber) { //change someNumber to real value
-		//call algorithm
+	FILE *outputFile = fopen(filename, "ab+");
+	fprintf(outputFile, "%d\n", totalCost);
+	
+	for (i = 0; i <= j; i++) {
+		fprintf(outputFile, "%d\n", path[i]);
 	}
 	
-	//... more if else statements for the number of algorithms to be thorough
-	
-	else {
-		//more fun stuff goes here
-	}
-	
-	FILE *outputFile = fopen(out, "ab+");
-	i = 0;
-	fprintf(outputFile, "%d\n", &ans);
-	
-	while (i <= j) {
-		fprintf(outputFile, "%d\n", &cities[i].id);
-	}
-	
-	fprintf(outputFile, "\n");
 	fclose(outputFile);
+
 	
 	return 0;
 }
